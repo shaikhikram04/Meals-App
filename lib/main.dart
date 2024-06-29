8d0783c0bba592d7edf9236eae8916c15374c2a4
Ikram Shaikh <ikramshaikh0480@gmail.com>
2024-06-29 20:54:31 +0530

    display category item on screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals/categories.dart';
+import 'package:meals/screens/categories.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
   brightness: Brightness.dark,
   colorScheme: ColorScheme.fromSeed(
-      seedColor: const Color.fromARGB(255, 131, 57, 0),
+      seedColor: const Color.fromARGB(255, 36, 16, 0),
       brightness: Brightness.dark),
   textTheme: GoogleFonts.latoTextTheme(),
 );
