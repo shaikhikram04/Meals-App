0f88ccbab9ab49e026e2177626b9904238e468fc
Ikram Shaikh <ikramshaikh0480@gmail.com>
2024-06-29 16:54:40 +0530

    create categories screen and add grid to it

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:meals/categories.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
   brightness: Brightness.dark,
   colorScheme: ColorScheme.fromSeed(
-    seedColor: const Color.fromARGB(255, 131, 57, 0),
-  ),
+      seedColor: const Color.fromARGB(255, 131, 57, 0),
+      brightness: Brightness.dark),
   textTheme: GoogleFonts.latoTextTheme(),
 );
 
@@ -16,24 +17,12 @@ void main() {
 
 class App extends StatelessWidget {
   const App({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
+      home: const CategoriesScreen(),
     );
   }
 }
