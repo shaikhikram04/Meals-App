469f1b7cbf15fd715fc552c41dc8c325810307d7
Ikram Shaikh <ikramshaikh0480@gmail.com>
2024-07-08 21:18:59 +0530

    Finetuning Explicit Animation

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -25,7 +25,7 @@ class _CategoriesScreenState extends State<CategoriesScreen>
 
     _animationController = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 300),
+      duration: const Duration(milliseconds: 350),
       lowerBound: 0,
       upperBound: 1,
     );
@@ -72,8 +72,12 @@ class _CategoriesScreenState extends State<CategoriesScreen>
             ),
         ],
       ),
-      builder: (context, child) => Padding(
-        padding: EdgeInsets.only(top: 100 - _animationController.value * 100),
+      builder: (context, child) => SlideTransition(
+        position: Tween(
+          begin: const Offset(0, 0.3),
+          end: const Offset(0, 0),
+        ).animate(CurvedAnimation(
+            parent: _animationController, curve: Curves.easeInOut)),
         child: child,
       ),
     );
